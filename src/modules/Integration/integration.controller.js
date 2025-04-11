681df28b7fa4864cec7814f14c8f072bdb7d5170
Mohamed-Hany1211 <m.searg1905@gmail.com>
2025-04-12 00:51:18 +0200

    working on ai request

--- src/modules/Integration/integration.controller.js
+++ src/modules/Integration/integration.controller.js
@@ -1,30 +1,51 @@
 import axios from "axios";
 import Vulns from '../../../DB/models/vulnerabilities.model.js';
 
+
 export const IntegrationApi = async (req,res,next) =>{
-    // 1 - get target website url
-    const {TargetUrl} = req.body;
-    // 2 - destructing the user id
-    // const {_id} = req.authUser;
-    // 3 - first request " the first request is for security api "
-    // 4 - {SecurityResponse} is the array to be sent to the AI api
-    const SecurityResponse = await axios.post(`http://localhost:3000/vulns/getAllVulnsWithUsersDummy`,TargetUrl);
-    const VulnsFound = SecurityResponse?.data.data;
-    return res.status(200).json({
-        data:VulnsFound
-    })
+    // // 1 - get target website url
+    // const {TargetUrl} = req.body;
+    // // 2 - destructing the user id
+    // // const {_id} = req.authUser;
+    // // 3 - first request " the first request is for security api "
+    // let baseURL = {
+    //     "url": TargetUrl,
+    // }
+    // // 4 - {SecurityResponse} is the array to be sent to the AI api
+    // const SecurityResponse = await axios.post(`http://localhost:3000/vulns/getAllVulnsWithUsersDummy`,baseURL);
+    // const VulnsFound = SecurityResponse?.data.data;
+    // return res.status(200).json({
+    //     data:VulnsFound
+    // })
     // ========================================================================================================
 
     // {if required we could perform any logic here}
 
     // ========================================================================================================
-    // 5 - second request " the second request is for the AI api "
-    // 6 - {AiResponse} is the array to be sent to the DB
-    // const AiResponse = await axios.post(`https://api.example.com/integration`,SecurityResponse);
+    // let rs = {
+    //     "vulnerabilities":[
+    //         "[swagger-api] [http] [info] http://localhost:3000/api-docs/swagger.json [paths=\"/api-docs/swagger.json\"]",
+    //         "[fingerprinthub-web-fingerprints:qm-system] [http] [info] http://localhost:3000",
+    //         "[http-missing-security-headers:referrer-policy] [http] [info] http://localhost:3000",
+    //         "[http-missing-security-headers:cross-origin-embedder-policy] [http] [info] http://localhost:3000",
+    //         "[http-missing-security-headers:strict-transport-security] [http] [info] http://localhost:3000",
+    //         "[http-missing-security-headers:x-permitted-cross-domain-policies] [http] [info] http://localhost:3000",
+    //         "[http-missing-security-headers:clear-site-data] [http] [info] http://localhost:3000",
+    //         "[robots-txt-endpoint] [http] [info] http://localhost:3000/robots.txt",
+    //         ]
+    // }
+    
+    // // 5 - second request " the second request is for the AI api "
+    // // 6 - {AiResponse} is the array to be sent to the DB
+    // const AiResponse = await axios.post(`http://localhost:4000/vulns_details`,rs);
+    // console.log(AiResponse);
+    
     // if(!AiResponse.length){
     //     return next({message:'no response from the AI api',cause:400});
     // }
-
+    // return res.stutus(200).json({
+    //     response:AiResponse
+    // })
     // 7 - db
     // const vulnsObject = {
     //     requestUserId:_id,
